8ff6ad58f3554b059904cf3a0efda4115275825b
Samuel Kevin Bagaskara <154964334+samuelkevinb@users.noreply.github.com>
2025-06-15 23:22:23 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer style="text-align: center; padding: 20px 10px; font-size: 14px; color: #444;">
+  <?= date('Y') ?> Bcrypt Hash Generator and Verifier
+  <p>Built with privacy in mind. No tracking, no cookies, no data collection</p>
+</footer>
